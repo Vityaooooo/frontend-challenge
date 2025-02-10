1dcd78cb3a4e82aaccccab29ab20f184139419cc
Ilyasov Viktor <ilasovviktor2666@gmail.com>
2025-02-10 20:55:10 +0300

    fix: svg fill

--- src/components/ui/card/CardUI.jsx
+++ src/components/ui/card/CardUI.jsx
@@ -2,7 +2,7 @@ import { ReactComponent as LikeButtonSvg } from '../../../images/LikeButton.svg'
 
 const CardUI = ({ cat, isLiked, isLoaded, onClick, onLoad }) => (
 	<article className={`card ${isLoaded ? 'card--loaded' : ''}`}>
-		<img className='card__image' src={cat.url} alt='cat' onLoad={onLoad} />
+		<img className={`card__image ${isLoaded ? 'card_loaded' : ''}`} src={cat.url} alt='cat' onLoad={onLoad}/>
 		<button
 			className={`card__button ${isLiked ? 'card__button_active' : ''}`}
 			onClick={onClick}>
